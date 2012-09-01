ecbce393ef11ffaf3697e72d42188a1bda0172a6
emanriquel <emanriquel@hotmail.com>
2012-09-01 18:25:19 -0500

    Disenio de marcado asistrencia beta

--- application/views/marcadoAsistencia.php
+++ application/views/marcadoAsistencia.php
@@ -4,34 +4,41 @@
 			<p>Huacho, 12 de Agosto del 2012</p>
 		</div>
 	</div>
-	<div class="marcado_asistencia_segundo">
-		<div class="asistencia_consulta">
-			<form> 
-				<label>DNI:</label>
-				<input type="number"/>
-			</form>
-		</div>	
+	<div class="marcado_asistencia_segundo">		
 		<div class="asistencia_hora_sistema">		
 			<p>08:10:15</p>
 		</div>
 	</div>
-	<div class="marcado_asistencia_tercero">
+	<div class="marcado_asistencia_tercero">	
+			<div class="asistencia_consulta">
+				<form> 
+					<div class="input-append">
+	  				<input class="span2" id="codigo_consultar" size="16" type="text">
+	  				<button class="btn" id="boton_consultar"type="button">Buscar</button>
+					</div>
+				</form>
+			</div>	
+	</div>			
+	<div class="marcado_asistencia_cuarto">		
 		<div class="asistencia_foto">
 			<div class="polaroid">				
 					<img src="<?php echo base_url("public/img/demo.jpg")?>"/>
 					<p>0333081003</p>
 			</div>
 		</div>
-		<div class="asistencia_datos">
-			
+		<div class="asistencia_datos">			
+			<div class="asistencia_mensaje">
+				<img src="<?php echo base_url("public/img/check.png")?>"/>
+				<p>ASISTENCIA REGISTRADA</p>
+			</div>
 			<div class="asistencia_ape_nom">
 				<p>MANRIQUE LEZAMETA, Elizabeth Cristina</p>
 			</div>
 			<div class="asistencia_car_cic">
 				<p>Ingeniería Informática - IX </p>
 			</div>
-			<div class="asistencia_mensaje">
-				<p>ASISTENCIA DUPLICADA</p>
+			<div class="asistencia_tipo">
+				<p>COMENSAL INSCRITO</p>
 			</div>
 		</div>
 	</div>
