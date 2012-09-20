c97859dcdf71b44f99041ee4499f6684c5b563cb
emanriquel <emanriquel@hotmail.com>
2012-09-19 22:48:44 -0500

    Enviando bd actualizada

--- application/views/beneficiado/registro_beneficiado.php
+++ application/views/beneficiado/registro_beneficiado.php
@@ -1,16 +1,16 @@
 <div class="panel_trabajo">
 	<div class="menu_registro_beneficiado">		
 		<ul class="nav nav-pills">
-		  <li class="active">
-		    <a href="#">Consulta</a>
+		  <li class="active link_menu_beneficiado">
+		    <a href="#"><i class="icon-list icon-black"></i>Consulta</a>
 		  </li>
-		   <li >
-		    <a href="#">Registro</a>
+		   <li class="link_menu_beneficiado" >
+		    <a href="#"><i class="icon-plus icon-black"></i>Nuevo Registro</a>
 		  </li>
-		  <li class="dropdown">
-		    <a class="dropdown-toggle"
+		  <li class="dropdown link_menu_beneficiado" >
+		    <a class="dropdown-toggle link_menu_beneficiado"
 		       data-toggle="dropdown"
-		       href="#">
+		       href="#"><i class="icon-print icon-black"></i>
 		        Exportar
 		        <b class="caret"></b>
 		      </a>
