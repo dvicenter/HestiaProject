a550add48a16041eae896903c7e7d0a9478c57ff
emanriquel <emanriquel@hotmail.com>
2012-09-21 19:06:54 -0500

    Reparado para linux

--- application/libraries/Control_Session.php
+++ application/libraries/Control_Session.php
@@ -2,7 +2,7 @@
 		if(!defined('BASEPATH'))
 		exit('No direct script access allowed');
 		
-		class Control_Session
+		class Control_session
 		{
 			private $ci;
 		    public function __construct()
