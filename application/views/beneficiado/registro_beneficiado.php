00f739ffe0dd7cf00054298969084f4f158028f8
emanriquel <emanriquel@hotmail.com>
2012-09-21 19:04:57 -0500

    AVANCE

--- application/views/beneficiado/registro_beneficiado.php
+++ application/views/beneficiado/registro_beneficiado.php
@@ -1,29 +1,35 @@
-<div class="panel_trabajo">
-	<div class="menu_registro_beneficiado">		
-		<ul class="nav nav-pills">
-		  <li class="active">
-		    <a href="#"class="link_menu_beneficiado"><i class="icon-list icon-black"></i>Consulta</a>
-		  </li>
-		   <li >
-		    <a href="#" class="link_menu_beneficiado"><i class="icon-plus icon-black"></i>Nuevo Registro</a>
-		  </li>
-		  <li class="dropdown" >
-		    <a class="dropdown-toggle link_menu_beneficiado"
-		       data-toggle="dropdown"
-		       href="#"><i class="icon-print icon-black"></i>
-		        Exportar
-		        <b class="caret"></b>
-		      </a>
-		    <ul class="dropdown-menu">
-		      <a>Excel</a>
-		      <a>Pdf</a>
-		    </ul>
-		  </li>
-		</ul>
-	</div>
+<div class="panel_trabajo">	
+	
+	
 	<div class="busqueda_registro_beneficiado">
-		
+	
+			<fieldset>
+			<legend>Búsqueda</legend>			
+			<div class="input-append busqueda_caja">
+			 	<label class="radio inline"> 
+			  	<input type="radio" value="">DNI
+				</label>
+			 	<label class="radio inline"> 
+			  		<input type="radio" value="">Apellidos y Nombres
+				</label>
+			  	<input class="span2" id="txt_consulta_beneficiado" size="100" type="text">
+			  	<button class="btn" type="input">Buscar</button>						 
+			</div>  
+			<div class="btn-group boton_exportar busqueda_caja">
+			  <a class="btn btn-info dropdown-toggle" data-toggle="dropdown" href="#">
+			    <i class="icon-print icon-white"></i>Exportar	
+			    <span class="caret"></span>
+			  </a>
+			  <ul style="top:initial" class="dropdown-menu">
+			    <li><a>PDF</a></li>
+			    <li><a>Excel</a></li>
+			  </ul>
+			</div>	
+		  	</fieldset>
 	</div>
+	<div class="menu_registro_beneficiado">		
+		 <button class="btn btn-primary" type="button"> <i class="icon-plus icon-white"></i>Nuevo Registro</button>
+	</div>	
 	<div class="tabla_registro_beneficiado">
 		
 	</div>
