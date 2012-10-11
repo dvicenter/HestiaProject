926a6347d1d4eedb6ef16a09b5f6a4c124000d80
unknown <Delmon@.(none)>
2012-10-10 20:21:35 -0500

    nuevo fpdf

--- application/controllers/beneficiado/gestion_beneficiado.php
+++ application/controllers/beneficiado/gestion_beneficiado.php
@@ -78,22 +78,40 @@
 			$this->load->model("beneficiado/gestion_beneficiado_model");
 			$data=$this->gestion_beneficiado_model->exportarBeneficiados('manrique',2);
 			$this->load->library('fpdf');
-			$this->fpdf->Open();
+			
+			/* COMIENZA */
+			
 			$this->fpdf->AddPage('P' , 'A4'); //mi formato de pagina 
-			$this->fpdf->SetFont('Arial','B',15);
+			$this->fpdf->SetFont('Arial','B',16);
 			$this->fpdf->Cell(80);
 			$this->fpdf->Image('http://4.bp.blogspot.com/_BSRFkkxuSEI/TDih0WqUGTI/AAAAAAAAGj4/fg0WTNspPxQ/s320/UNJFSC.png',10,10,-300,'PNG').
 			// Framed title
 			$title= utf8_decode("Universidad Nacional José Faustino Sánchez Carrión");
 		    $this->fpdf->Cell(30,10,$title,0,0,'C');
+		    $this->fpdf->Ln();
+			$this->fpdf->SetFont('Arial','B',12);
+			$this->fpdf->Cell(80);
+			$this->fpdf->Cell(30,10,"UNIVERSIDAD DE COMEDOR UNIVERSITARIO",0,0,'C');
+		    
 		    // Line break
-		    $this->fpdf->Ln(20);
-			$this->fpdf->SetFont('Arial','',10); //mi fuente
+		    $this->fpdf->Ln();
+			$this->fpdf->SetFonT('Arial','',10); //mi fuente
+			$this->fpdf->Ln();
+			
+			//CABECERA TABLA
+			$this->fpdf->Cell(20,5,'DNI',1,0,'C'); 
+			$this->fpdf->Cell(80,5,'APELLIDOS Y NOMBRES',1,0,'C'); 
+			$this->fpdf->Cell(50,5,'CARRERA PROFESIONAL',1,0,'C'); 
+			$this->fpdf->Cell(20,5,'CICLO',1,0,'C'); 
+			$this->fpdf->Cell(25,5,utf8_decode('CONDICIÓN'),1,0,'C');
+			$this->fpdf->Ln(); // salto de linea		
+			
+			
 			foreach ($data as $resultado){ //usando los datos de mysql
 			$this->fpdf->Cell(20,5,$resultado['dni'],1,0,'C'); 
 			$this->fpdf->Cell(80,5,$resultado['nombrescompletos'],1,0,'L'); 
 			$this->fpdf->Cell(50,5,$resultado['nombrecarreraprofesional'],1,0,'C'); 
-			$this->fpdf->Cell(10,5,$resultado['numciclo'],1,0,'C'); 
+			$this->fpdf->Cell(20,5,$resultado['numciclo'],1,0,'C'); 
 			$this->fpdf->Cell(25,5,$resultado['condicionfinal'],1,0,'C');
 			$this->fpdf->Ln(); // salto de linea		
 			}
@@ -103,14 +121,94 @@
 		    // Print centered page number
 		    date_default_timezone_set('Etc/GMT+5');
 			// fecha
-			$foot = utf8_decode("Fecha de impresión:".date("Y-m-d H:i:s"));
+			$foot = utf8_decode("Reporte emitido por Hestia - Fecha de impresión:".date("Y-m-d H:i:s"));
 			
-		    $this->fpdf->Cell(0,5,$foot,1,0,'C');
+		    //$this->fpdf->Cell(0,5,$foot,1,0,'C');
 			//
-			$this->fpdf->Cell(0,5,'Pagina '.$this->fpdf->PageNo(),1,0,'R');
+			//$this->fpdf->Cell(0,5,'Pagina '.$this->fpdf->PageNo(),1,0,'R');
+			$this->fpdf->SetY(-15);
+		    //Arial italic 8
+    		$this->fpdf->SetFont('Arial','I',8);
+		    //Número de página
+		    $this->fpdf->Cell(0,10,'Page '.$this->fpdf->PageNo().'/{nb}',0,0,'C');
 			//finaliza y muestra en pantalla pdf
 			$this->fpdf->Output(); // si se deja Output() asi "solo" el archivo al guardarlo tiene el nombre doc.pdf y el parametro 'D' obliga a guardarlo , que era lo que yo necesitaba!!
 			
+	}
+
+	function Header()
+    {
+	    //Logo
+	    $this->Image("http://4.bp.blogspot.com/_BSRFkkxuSEI/TDih0WqUGTI/AAAAAAAAGj4/fg0WTNspPxQ/s320/UNJFSC.png" , 10 ,8, 35 , 38 , "JPG" ,"http://www.mipagina.com");
+	    //Arial bold 15
+	    $this->SetFont('Arial','B',15);
+	    //Movernos a la derecha
+	    $this->Cell(80);
+	    //Título
+	    $this->Cell(60,10,'Titulo del archivo',1,0,'C');
+	    //Salto de línea
+	    $this->Ln(20);
+	}
+   
+	   //Pie de página
+	function Footer()
+	{
+	    //Posición: a 1,5 cm del final
+	    $this->fpdf->SetY(-15);
+	    //Arial italic 8
+	    $this->fpdf->SetFont('Arial','I',8);
+	    //Número de página
+	   	$this->fpdf->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+	}
+      //Tabla coloreada
+	function TablaColores($header)
+	{
+		//Colores, ancho de línea y fuente en negrita
+		$this->fpdf->SetFillColor(255,0,0);
+		$this->fpdf->SetTextColor(255);
+		$this->fpdf->SetDrawColor(128,0,0);
+		$this->fpdf->SetLineWidth(.3);
+		$this->fpdf->SetFont('Arial','B');
+		//Cabecera
+		for($i=0;$i<count($header);$i++)
+		$this->fpdf->Cell(40,7,$header[$i],1,0,'C',1);
+		$this->fpdf->Ln();
+		//Restauración de colores y fuentes
+		$this->fpdf->SetFillColor(224,235,255);
+		$this->fpdf->SetTextColor(0);
+		$this->fpdf->SetFont('');
+		//Datos
+		$fill=false;
+		$this->fpdf->Cell(40,6,"hola",'LR',0,'L',$fill);
+		$this->fpdf->Cell(40,6,"hola2",'LR',0,'L',$fill);
+		$this->fpdf->Cell(40,6,"hola3",'LR',0,'R',$fill);
+		$this->fpdf->Cell(40,6,"hola4",'LR',0,'R',$fill);
+		$this->fpdf->Ln();
+		$fill=!$fill;
+		$this->fpdf->Cell(40,6,"col",'LR',0,'L',$fill);
+		$this->fpdf->Cell(40,6,"col2",'LR',0,'L',$fill);
+		$this->fpdf->Cell(40,6,"col3",'LR',0,'R',$fill);
+		$this->fpdf->Cell(40,6,"col4",'LR',0,'R',$fill);
+		$fill=true;
+		$this->fpdf->Ln();
+		$this->fpdf->Cell(160,0,'','T');
+	}
+   public function exportarBeneficiado2()
+	{
+			$this->load->library('fpdf');
+			//$pdf=$this->fpdf->PDF();
+			//Títulos de las columnas
+			$header=array('Columna 1','Columna 2','Columna 3','Columna 4');
+			$this->fpdf->AliasNbPages();
+			//Primera página
+			$this->fpdf->AddPage();
+			$this->fpdf->SetY(65);
+			//$pdf->AddPage();
+			$this->TablaColores($header);
+			//Segunda página
+			$this->fpdf->AddPage();
+			$this->fpdf->SetY(65);
+			$this->fpdf->Output();			
 	}
 	
 }
