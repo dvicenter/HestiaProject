0a3a606ddd51ff5b0bd9cd400b76a02efe8f701e
emanriquel <emanriquel@hotmail.com>
2012-10-14 18:24:57 -0500

    ansi

---
+++ application/libraries/control_session.php
@@ -0,0 +1,25 @@
+ <?php
+		if(!defined('BASEPATH')) exit('No direct script access allowed');
+		
+		class Control_session
+		{
+			private $ci;
+		    public function __construct()
+		    {		    	
+		        $this->ci = get_instance();
+		     	$this->ci ->load->library('session');
+		    }
+			public function verificarSesionConRedireccionamientoLogin(){
+				if ($this->ci ->session->userdata('Validado')!='true') 
+		        {        	     	
+					redirect('login', 'refresh');
+				}
+			}
+			public function verificarSesionConRedireccionamientoHome(){
+				if ($this->ci ->session->userdata('Validado')=='true') 
+		        {        	     	
+					redirect('home', 'refresh');
+				}
+			}
+		} 
+?>
