813a15406ebaabced25f9236c4726e28dd3250b0
delmon <delmonxd@gmail.com>
2012-10-13 20:28:43 -0500

    added excel fix

--- application/controllers/beneficiado/gestion_beneficiado.php
+++ application/controllers/beneficiado/gestion_beneficiado.php
@@ -97,10 +97,12 @@
         $this->tcpdf->Output('ReporteHestia.pdf', 'I'); 
      }
 	public function exportarBeneficiadoExcel(){
+		
 		header("Content-Type: application/vnd.ms-excel");
 		header("Expires: 0");
 		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 		header("content-disposition: attachment;filename=Reportes.xls");
+		//LLama helper
 		$this->load->helper('tcpdf_helper');
 		$parametro=$this->input->get('txt_consulta_beneficiado',TRUE)."";
 		$parametro = 'pa';
